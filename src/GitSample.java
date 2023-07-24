db5b028805f422c1b5bf0682b6392458fc7b501d
ID042517 <ashish.sharma@external.t-mobile.nl>
2023-07-24 21:52:30 +0530

    Second Branch first commit

--- src/GitSample.java
+++ src/GitSample.java
@@ -3,7 +3,7 @@ public class GitSample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.println("Git sample branch method");
+      System.out.println("Git sample seond branch method");
 	}
 
 }
